c808765b3ec852144e5c3a7b79a0a440450316d2
chengheai <842385139@qq.com>
2018-09-10 17:09:25 +0800

    Signed-off-by: chengheai <842385139@qq.com>

---
+++ requirejs/a.js
@@ -0,0 +1,18 @@
+// function fun1(){
+//   alert('it works');
+// }
+// fun1();
+//#endregion
+// (function(){
+//   function fun1() {
+//     alert('it works')
+//   }
+//   fun1()
+// })()
+
+define(function(){
+  function fun1() {
+    alert('it works');
+  }
+  fun1();
+})
